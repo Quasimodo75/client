3089d0cfe1517eeaccdc03f3002d6b12f7614ee4
Qausimodo75 <kwazikubheka777@gmail.com>
2023-09-14 09:35:46 +0200

    Buttom form is working with all the styling

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent {
+  loginForm = new FormGroup({
+    Email: new FormControl(''),
+    Password: new FormControl(''),
+  });
 
+ 
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.loginForm.value);
+  }
 }
